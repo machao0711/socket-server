b0c5b93e44c17feed45d1c15b4c5f75fe3fd3e83
machao0711 <18538526536@163.com>
2018-08-22 11:45:46 +0800

    test

--- src/test/java/com/demo/project/SocketServerApplicationTests.java
+++ src/test/java/com/demo/project/SocketServerApplicationTests.java
@@ -1,5 +1,7 @@
 package com.demo.project;
 
+import java.net.InetAddress;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SocketServerApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		//获取本机的InetAddress实例
+		InetAddress address =InetAddress.getLocalHost();
+		String name=address.getHostName();//获取计算机名
+		System.out.println(name);
+		String ip=address.getHostAddress();//获取IP地址
+		System.out.println(ip);
+		byte[] bytes = address.getAddress();//获取字节数组形式的IP地址,以点分隔的四部分
+		System.out.println(bytes.toString());
+		//获取其他主机的InetAddress实例
+		//InetAddress address2 =InetAddress.getByName("其他主机名");
+		//InetAddress address3 =InetAddress.getByName("IP地址");
 	}
 
 }
